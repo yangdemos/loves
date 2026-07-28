3806b5a7ac36ec16b7cd8d8a07390281661a0f12
yangkunshuo <2454864580@qq.com>
2026-07-28 23:37:12 +1200

    chore: purge temp, test, and scratch artifacts from tracking

--- script.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let lines = fs.readFileSync('js/main.js', 'utf8').split('
-');
-
-const newSection = [];
-newSection.push('  // --- 3D Round Carousel (always auto-rotating + drag momentum) ---');
-newSection.push('  const carouselContainer = document.getElementById(\"carouselContainer\");');
-console.log('test ok');
